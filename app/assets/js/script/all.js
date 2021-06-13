85be7d31d59c26fd8503f2be01b8d2e280560e4f
lucas701780 <lucas701780@gmail.com>
2021-06-13 11:51:36 +0800

    1.完成切版練習

--- app/assets/js/script/all.js
+++ app/assets/js/script/all.js
@@ -5,3 +5,31 @@ $(document).ready(function () {
       $('.menu').toggleClass('active');
   })
 });
+
+$(document).ready(function () {
+  // 漢堡選單
+  $('.blog-mobile-menu').click(function (e) {
+      e.preventDefault();
+      $('.blogl-menu-list').toggleClass('active');
+      $('.blog-nav').toggleClass('active');
+  })
+});
+
+// 隱藏所有的 li 內文
+$('.faq-card-item-body').hide();
+// 預設讓第一個 li 加上 active
+$('.faq-card-item').eq(0).addClass('active');
+// 預設讓第一個 li 內文 顯示
+$('.faq-card-item-body').eq(0).show();
+
+// 點擊 li
+$('.faq-card-item').click(function (e) {
+  // 取消預設 event 事件
+  e.preventDefault(); 
+  // 加上 active 屬性，並把其他有 active 的移除
+  $(this).toggleClass('active').siblings().removeClass('active');
+  // 讓自己的 li body 切換收合
+  $(this).children().next().slideToggle();
+  // 讓其他 li body 收合
+  $(this).siblings().children().next().slideUp();
+})
